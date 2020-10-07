dc816f5f70d6f5b3dcfad42a2e0532b50b5d4066
益子大空 <oosky@Nyanpaso.local>
2020-10-07 16:59:31 +0900

    2020-10-07

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.listen(4000);
